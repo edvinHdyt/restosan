16cafe3210dd0c48fde3614538cc4b41fca5be5b
zdn2442 <zdn.2442.ok@gmail.com>
2025-12-17 19:16:39 +0700

    zaidan: detailing & responsive dashboard

--- controller/controller.js
+++ controller/controller.js
@@ -15,3 +15,23 @@ window.onclick = function (event) {
     }
   }
 };
+
+function slideRight(btn) {
+        const slider = btn.parentElement.querySelector('.slider-container');
+        const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+        if (slider.scrollLeft >= maxScrollLeft - 5) {
+            slider.scrollTo({ left: 0, behavior: 'smooth' });
+        } else {
+            slider.scrollBy({ left: slider.clientWidth, behavior: 'smooth' });
+        }
+    }
+
+    function slideLeft(btn) {
+        const slider = btn.parentElement.querySelector('.slider-container');
+        const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+        if (slider.scrollLeft <= 5) {
+            slider.scrollTo({ left: maxScrollLeft, behavior: 'smooth' });
+        } else {
+            slider.scrollBy({ left: -slider.clientWidth, behavior: 'smooth' });
+        }
+    }
