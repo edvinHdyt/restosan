8af8b7342b0075165d075235591f82dddfe28cd2
edvinHdyt <edvinhidayat@gmail.com>
2025-12-20 16:04:36 +0700

    creating dropdown navbar and profile

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,113 @@
+document.addEventListener("click", function(){
+  const strId = event.target.dataset['id'];
+
+  switch (strId) {
+    case "showDropdownNavbar":
+        manipulateDropdownNavbar();
+        hideDropdownProfile();
+      break;
+    case "showDropdownProfile":
+        manipulateDropdownProfile();
+        hideDropdownNavbar();
+      break;
+    default:
+      break;
+  }
+});
+
+function hideDropdownNavbar(){
+   const elm = document.getElementById("dropdownNavbar");
+
+  if (!elm.classList.contains("opacity-0")){
+    elm.classList.remove("translate-y-[-10px]");
+    elm.classList.remove("animate-slidedown");
+    elm.classList.remove("opacity-100");
+    elm.classList.add("animate-slideup");
+
+    timeout = setTimeout(()=> {
+      elm.classList.add("translate-y-[-300px]");
+      elm.classList.add("opacity-0");
+    }, 200);
+  }
+}
+
+function hideDropdownProfile(){
+    const elm = document.getElementById("dropdownProfile");
+    const elmParent = document.getElementById("parentDropdownProfile");
+
+    if (!elm.classList.contains("opacity-0")){
+        elm.classList.add("animate-profile-slidedown");
+        elm.classList.remove("opacity-100");
+        elm.classList.remove("animate-profile-slideup");
+
+        setTimeout(() => {
+          elm.classList.add("opacity-0");
+          elm.classList.add("translate-y-20");
+          elmParent.classList.remove('z-100');
+          elmParent.classList.add('z-0');
+        }, 100);
+    }
+}
+
+function manipulateDropdownNavbar(){
+  const elm = document.getElementById("dropdownNavbar");
+
+  let timeout = null;
+  if (elm.classList.contains("opacity-0")){
+    elm.classList.remove("translate-y-[-300px]");
+    elm.classList.remove("animate-slideup");
+    elm.classList.remove("opacity-0");
+    elm.classList.add("translate-y-[-10px]");
+    elm.classList.add("opacity-100");
+    elm.classList.add("animate-slidedown");
+    
+    timeout != null ? clearTimeout(timeout) : "";
+  } else {
+    elm.classList.remove("translate-y-[-10px]");
+    elm.classList.remove("animate-slidedown");
+    elm.classList.remove("opacity-100");
+    elm.classList.add("animate-slideup");
+
+    timeout = setTimeout(()=> {
+      elm.classList.add("translate-y-[-300px]");
+      elm.classList.add("opacity-0");
+    }, 200);
+  }
+}
+
+
+function manipulateDropdownProfile(){
+  const elm = document.getElementById("dropdownProfile");
+  const elmParent = document.getElementById("parentDropdownProfile");
+  let timeout = null;
+
+  if (elm.classList.contains("opacity-0")){
+    elm.classList.remove("opacity-0");
+    elm.classList.remove("translate-y-20");
+    elm.classList.remove("animate-profile-slidedown");
+    elm.classList.add("opacity-100");
+    elm.classList.add("animate-profile-slideup");
+
+    elmParent.classList.remove('z-0');
+    elmParent.classList.add('z-100');
+
+    timeout != null ? clearTimeout(timeout) : "";
+  }else{
+    elm.classList.add("animate-profile-slidedown");
+    elm.classList.remove("opacity-100");
+    elm.classList.remove("animate-profile-slideup");
+
+    
+
+    setTimeout(() => {
+      elm.classList.add("opacity-0");
+      elm.classList.add("translate-y-20");
+      elmParent.classList.remove('z-100');
+      elmParent.classList.add('z-0');
+    }, 100);
+  }
+}
+
 function toggleDropdown() {
   const menu = document.getElementById("kotaMenu");
   if (menu.classList.contains("hidden")) {
@@ -10,7 +120,7 @@ function toggleDropdown() {
 window.onclick = function (event) {
   if (!event.target.closest("#kotaBtn")) {
     const menu = document.getElementById("kotaMenu");
-    if (!menu.classList.contains("hidden")) {
+    if (!menu == null && !menu.classList.contains("hidden")) {
       menu.classList.add("hidden");
     }
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 @type {import('tailwindcss').Config} 
+
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        slidedown: "slidedown 0.5s ease-in",
+        slideup: "slideup 0.5s ease-out"
+      }
+    },
+    keyFrames:{
+      slidedown: {
+        "0%": {
+          transform: "translateY(-200px)",
+          opacity: "0"
+        },
+        "100%": {
+          transform: "translateY(-10px)",
+          opacity: "100"
+        }
+      },
+      slideup: {
+        "0%": {
+          transform: "translateY(-10px)",
+          opacity: "100"
+        },
+        "100%": {
+          transform: "translateY(-200px)",
+          opacity: "0"
+        }
+      }
+    }
   },
   plugins: [],
 }
